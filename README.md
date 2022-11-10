40d874b985673dc62849854b7381967a6fda8565
Martín <magadiflo@gmail.com>
2022-11-10 10:54:37 -0500

    Proyecto base - Con dependencia base: Eureka Server

---
+++ README.md
@@ -0,0 +1,3 @@
+# Microservicios Spring Cloud Eureka & Angular Full-stack
+
+## Sección 3: Backend: Eureka Server registrando microservicios
---
+++ src/main/java/com/magadiflo/eureka/FsMicroservicioEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.magadiflo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsMicroservicioEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsMicroservicioEurekaApplication.class, args);
+	}
+
+}
